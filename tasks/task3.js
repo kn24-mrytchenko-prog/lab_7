be3dfacb024bc7d25932e9b8268554548866b4e6
kn24-mrytchenko-prog <kn24-m.rytchenko@nubip.edu.ua>
2025-10-27 16:25:47 +0200

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,14 +1,17 @@
-"Ваш код повинен зробити PATCH-запит до вказаного URL, де {userId} – це ID існуючого користувача."
-"Для оновлення користувача передайте в запит нові дані, наприклад, нове ім’я."
-"Поверніть відповідь від сервера з оновленими даними користувача."
+async function updateUser(id, updatedData) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(updatedData)
+  });
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
-
-function updateUser(id, updatedData) {
-  // Ваш код
+  const data = await response.json();
+  return data;
 }
 
-console.log(updateUser(1, { name: 'New Name' }));
-
 module.exports = updateUser;
+
+// Тест виклику
+updateUser(1, { name: "New Name" }).then(console.log);
